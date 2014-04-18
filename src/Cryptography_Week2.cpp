c91cc2c4c6ba34a673a29a965facf2835833facd
Andrew Snyder <andrew.snyder.email@gmail.com>
2014-04-18 19:17:40 -0400

    Grabbed the example from the crypto lib.

--- src/Cryptography_Week2.cpp
+++ src/Cryptography_Week2.cpp
@@ -7,7 +7,34 @@
 //============================================================================
 
 #include <iostream>
-#include "aes.h"
+using std::cout;
+using std::cerr;
+using std::endl;
+
+#include <string>
+using std::string;
+
+#include <aes.h>
+using CryptoPP::AES;
+
+#include "cryptlib.h"
+using CryptoPP::Exception;
+
+#include "hex.h"
+using CryptoPP::HexEncoder;
+using CryptoPP::HexDecoder;
+
+#include "filters.h"
+using CryptoPP::StringSink;
+using CryptoPP::StringSource;
+using CryptoPP::StreamTransformationFilter;
+
+#include "ccm.h"
+using CryptoPP::CBC_Mode;
+
+#include "osrng.h"
+using CryptoPP::AutoSeededRandomPool;
+
 using namespace std;
 
 const int cbcKey1[] = {0x14, 0x0b, 0x41, 0xb2, 0x2a, 0x29, 0xbe, 0xb4, 0x06, 0x1b,
@@ -46,8 +73,87 @@ const int ctrCipher2[] = {0x77, 0x0b, 0x80, 0x25, 0x9e, 0xc3, 0x3b, 0xeb, 0x25,
       0x51};
 
 
-int main() {
-   AESEncryption a;
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
-	return 0;
+int main()
+{
+   AutoSeededRandomPool prng;
+
+   byte key[AES::DEFAULT_KEYLENGTH];
+   prng.GenerateBlock(key, sizeof(key));
+
+   byte iv[AES::BLOCKSIZE];
+   prng.GenerateBlock(iv, sizeof(iv));
+
+   string plain = "CBC Mode Test";
+   string cipher, encoded, recovered;
+
+   /*********************************\
+      \*********************************/
+
+   // Pretty print key
+   encoded.clear();
+   StringSource(key, sizeof(key), true, new HexEncoder(new StringSink(encoded)) // HexEncoder
+      );// StringSource
+   cout << "key: " << encoded << endl;
+
+   // Pretty print iv
+   encoded.clear();
+   StringSource(iv, sizeof(iv), true, new HexEncoder(new StringSink(encoded)) // HexEncoder
+      );// StringSource
+   cout << "iv: " << encoded << endl;
+
+   /*********************************\
+      \*********************************/
+
+   try
+   {
+      cout << "plain text: " << plain << endl;
+
+      CBC_Mode<AES>::Encryption e;
+      e.SetKeyWithIV(key, sizeof(key), iv);
+
+      // The StreamTransformationFilter removes
+      //  padding as required.
+      StringSource s(plain, true,
+         new StreamTransformationFilter(e, new StringSink(cipher)) // StreamTransformationFilter
+            );// StringSource
+   } catch (const CryptoPP::Exception& e)
+   {
+      cerr << e.what() << endl;
+      exit(1);
+   }
+
+   /*********************************\
+      \*********************************/
+
+   // Pretty print
+   encoded.clear();
+   StringSource(cipher, true, new HexEncoder(new StringSink(encoded)) // HexEncoder
+      );// StringSource
+   cout << "cipher text: " << encoded << endl;
+
+   /*********************************\
+      \*********************************/
+
+   try
+   {
+      CBC_Mode<AES>::Decryption d;
+      d.SetKeyWithIV(key, sizeof(key), iv);
+
+      // The StreamTransformationFilter removes
+      //  padding as required.
+      StringSource s(cipher, true,
+         new StreamTransformationFilter(d, new StringSink(recovered)) // StreamTransformationFilter
+            );// StringSource
+
+      cout << "recovered text: " << recovered << endl;
+   } catch (const CryptoPP::Exception& e)
+   {
+      cerr << e.what() << endl;
+      exit(1);
+   }
+
+   /*********************************\
+   \*********************************/
+
+   return 0;
 }
